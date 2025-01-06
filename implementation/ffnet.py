8f425865ff1af47351c069f34d40c35924a2b954
dkytezab <danielkytezable@gmail.com>
2025-01-06 12:25:01 -0500

    Update ffnet.py

--- implementation/ffnet.py
+++ implementation/ffnet.py
@@ -13,6 +13,7 @@ class MLP(nn.Module):
             hidden_width: int,
             activ_fn=nn.ReLU
     ) -> None:
+
         super(MLP, self).__init__()
         layers = []
         cur_dim = input_dim
@@ -24,30 +25,35 @@ class MLP(nn.Module):
         
         self.hidden_layers = nn.Sequential(*layers)
         self.output_layer = nn.Linear(cur_dim, output_dim)
+
     
     def forward(
             self, 
             input: torch.Tensor
     ) -> torch.Tensor:
+
         hidden_output = self.hidden_layers(input)
         return self.output_layer(hidden_output)
+
     
     def train(
             model, 
             train_loader: torch.Tensor, 
             epochs: int, 
-            learning_rate=0.01
+            learning_rate=0.005
     ) -> None:
+
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         for step in range(epochs):
             for in_batch, out_batch in train_loader:
                 optimizer.zero_grad()
                 outputs = model(in_batch)
-                loss = torch.mean((out_batch - in_batch) ** 2)
+                loss = torch.mean((outputs - out_batch) ** 2)
                 loss.backward()
                 optimizer.step()
 
-class FeatureEmbedding():
+
+class FourierFeatureEmbedding():
     def __init__(
             self,
             input_dim: int,
@@ -55,28 +61,38 @@ class FeatureEmbedding():
             kernel: str,
             length_scale: float
     ) -> None:
+        '''Computes random features for embedding'''
+
         size = (num_features, input_dim)
 
+        if kernel == None:
+          random_mat = np.eye(size=size)
+
         if kernel == 'RBF':
-            '''corresponding spectral kernel is Gaussian with mean=0, variance=1/length_scale**2'''
+            '''Corresponding spectral kernel is Gaussian with mean=0, variance=1/length_scale**2'''
             random_mat = np.random.normal(loc=0, 
                                           scale=1 / length_scale, 
                                           size=size)
         
         if kernel == 'Laplace':
-            '''corresponding spectral kernel is Cauchy with loc=0, scale=length_scale'''
+            '''Corresponding spectral kernel is Cauchy with loc=0, scale=1/length_scale'''
             random_mat = sp.stats.cauchy.rvs(loc=0, 
-                                             scale=length_scale, 
+                                             scale=1 / length_scale, 
                                              size=size)
         self.random_features = random_mat
         self.num_features = num_features
 
+    
     def embed(
             self,
             input: np.ndarray
     ) -> torch.Tensor:
+        '''Embeds a given dataset using the computed random features'''
+
         cos_mat = np.cos(2 * np.pi * self.random_features @ input.T)
         sin_mat = np.sin(2 * np.pi * self.random_features @ input.T)
-        
-        return np.concatenate(
+
+        embed_mat = np.concatenate(
             (cos_mat, sin_mat), axis=0).T / np.sqrt(self.num_features)
+        
+        return torch.tensor(embed_mat, dtype=torch.float32)
