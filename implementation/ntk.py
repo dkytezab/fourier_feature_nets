fff322d0c977d1633e5ee20da308b75189ca87d5
dkytezab <danielkytezable@gmail.com>
2025-01-09 11:54:49 -0500

    Update ntk.py to better reflect dataset shape

--- implementation/ntk.py
+++ implementation/ntk.py
@@ -3,16 +3,16 @@ import torch.nn as nn
 import torch.autograd as autograd
 import numpy as np
 
-def grad(model, 
+def grad(model,
          input: torch.Tensor
 ) -> torch.Tensor:
-  
+
   params = list(model.parameters())
   model.zero_grad()
   output = model(input)
   output.backward(
       torch.ones_like(output))
-  return torch.cat([param.grad.view(-1) for param in params 
+  return torch.cat([param.grad.view(-1) for param in params
                     if param.requires_grad])
 
 
@@ -22,12 +22,14 @@ class NTK():
                input_loader: torch.Tensor
   ) -> None:
 
-    num_samples = input_loader.tensors[0].shape[0]
+    dataset = input_loader.dataset
+
+    num_samples = dataset[:, 0][0].numpy().shape[0]
     emp_matrix = torch.zeros((num_samples, num_samples))
 
-    grads = [grad(model, input_loader.tensors[0][idx]) 
+    grads = [grad(model, dataset[idx][0])
              for idx in range(num_samples)]
-    
+
     for row in range(num_samples):
       for col in range(row, num_samples):
         emp_matrix[row, col] = \
